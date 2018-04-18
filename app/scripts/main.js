93de1fccaa23f8212a7ff273f061f88d1f4c5f1a
Robert Faskhutdinov <Robert_Faskhutdinov@epam.com>
2018-04-18 16:16:50 +0200

    test1

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,10 +1,11 @@
 "use strict";
 import square from  './t2.js';
+var test1 = 0;
 
 var r = function(){
- let t = 454;
-  let c = square(t + 2);
- console.log(c + c);
+ let t = 454; test1++;
+  let c = square(t); test1++;
+ console.log(c + c + c);
 };
 
 r();
